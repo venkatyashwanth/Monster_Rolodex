4c0944c24a35aeff0460825ba3ce3789eb8942cf
venkat yashwanth <mudiliyashwanth@gmail.com>
2024-02-06 06:45:44 +0530

    search box full functionality

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
   }
 
@@ -24,10 +25,23 @@ class App extends Component {
 
   render() {
     const monstersList = this.state.monsters;
+    const filterMonsters = monstersList.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    })
+
     return (
       <div>
-        <input className='search-box' type='search' placeholder='search monsters' onChange={(event)=>{console.log(event.target.value)}}/>
-        {monstersList.map((monster) => {
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={(event) => {
+            const searchField = event.target.value.toLowerCase();
+            this.setState(()=> {
+              return {searchField}
+            })
+          }} />
+        {filterMonsters.map((monster) => {
           return <h1 key={monster.id}>{monster.name}</h1>;
         })}
       </div>
